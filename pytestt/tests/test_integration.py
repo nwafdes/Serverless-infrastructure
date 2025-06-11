411ac790f94125bf5180f1577bafb69d735f431e
unknown <136354426+nwafdes@users.noreply.github.com>
2025-06-12 00:18:48 +0300

    changed key in test
    Signed-off-by: unknown <136354426+nwafdes@users.noreply.github.com>

--- pytestt/tests/test_integration.py
+++ pytestt/tests/test_integration.py
@@ -4,7 +4,7 @@ import os
 
 api_url = "https://60h2ko3a49.execute-api.us-east-1.amazonaws.com/prod/visitors"
 headers = {
-    "x-api-key": 'I8QYNljUps2cpMILlq9mZ9KnI3jTlKpS8sanuHv'
+    "x-api-key": 'I8QYNljUps2cpMILlq9mZ9KnI3jTlKpS8sanuHvD'
 }
 def test_get():
     response = requests.get(api_url,headers=headers)
