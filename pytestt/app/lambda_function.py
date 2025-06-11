9acf8cdfb48b725bbdcf6d2da376a0df7b7e12b0
unknown <136354426+nwafdes@users.noreply.github.com>
2025-06-11 17:53:18 +0300

    update: Updated lambda file
    Signed-off-by: unknown <136354426+nwafdes@users.noreply.github.com>

--- pytestt/app/lambda_function.py
+++ pytestt/app/lambda_function.py
@@ -138,3 +138,5 @@ def lambda_handler(event, context):
             'statusCode': 500,
             'body': json.dumps({"Error": "WE are Facing an error!!"})
         }
+
+# tests code for the 5th time
